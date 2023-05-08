ad6eb7f146caef58f24b4f6b54320f299614522d
Carlos Ruiz Lopez <ruizlo.locar23@triana.salesianos.edu>
2023-05-08 20:18:53 +0200

    actualizacion proyecto

---
+++ src/main/java/com/example/demo/controller/ClienteController.java
@@ -0,0 +1,29 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import  com.example.demo.model.Cliente;
+
+@Controller
+public class ClienteController {
+
+	@GetMapping("/cliente")
+	public String showForm(Model model) {
+		
+		Cliente cliente = new Cliente();
+		model.addAttribute("clienteForm", cliente);
+		
+		return "portada";
+		
+	}
+	
+	public String submit(@ModelAttribute("clienteForm") Cliente cliente, Model model) {
+		
+		model.addAttribute("cliente", cliente);
+		
+		return "view";
+	}
+}
