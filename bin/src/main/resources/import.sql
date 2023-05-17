ef47c166f1aa8f2ccdbc00b76530851e5c8e6348
Carlos Ruiz Lopez <ruizlo.locar23@triana.salesianos.edu>
2023-05-17 20:43:23 +0200

    New changes

--- bin/src/main/resources/import.sql
+++ bin/src/main/resources/import.sql
@@ -1,21 +1,31 @@
-INSERT INTO menu (cod_menu, nombre, descripcion, precio) VALUES
-	(1, 'Menú Ejecutivo', 'Incluye una selección de platos gourmet y postre', 25.99),
-	(2, 'Menú Vegetariano', 'Incluye una selección de platos vegetarianos y postre', 23.99),
-	(3, 'Menú Infantil', 'Incluye plato principal, bebida y postre para los más pequeños', 12.99),
-	(4, 'Menú Mediterráneo', 'Incluye una selección de platos mediterráneos y postre', 27.99),
-	(5, 'Menú Italiano', 'Incluye una selección de platos italianos y postre', 29.99),
-	(6, 'Menú Asiático', 'Incluye una selección de platos asiáticos y postre', 24.99),
-	(7, 'Menú Buffet', 'Incluye una variedad de platos fríos y calientes para servir estilo buffet', 32.99),
-	(8, 'Menú BBQ', 'Incluye una selección de carnes y acompañamientos para una barbacoa', 35.99),
-	(9, 'Menú de Mariscos', 'Incluye una selección de mariscos frescos y postre', 39.99),
-	(10, 'Menú Saludable', 'Incluye una selección de platos saludables y postre', 26.99),
-	(11, 'Menú Gourmet', 'Incluye una selección de platos gourmet y postre', 44.99),
-	(12, 'Menú Navideño', 'Incluye una selección de platos navideños y postre', 38.99),
-	(13, 'Menú para Fiestas', 'Incluye una variedad de platos fríos y calientes para fiestas', 29.99),
-	(14, 'Menú para Reuniones', 'Incluye una variedad de platos fríos y calientes para reuniones', 28.99),
-	(15, 'Menú para Bodas', 'Incluye una selección de platos elegantes para bodas y postre', 49.99),
-	(16, 'Menú para Cumpleaños', 'Incluye una selección de platos y postre para cumpleaños', 21.99),
-	(17, 'Menú para Aniversarios', 'Incluye una selección de platos y postre para aniversarios', 34.99),
-	(18, 'Menú de Tapas', 'Incluye una selección de tapas y postre', 19.99),
-	(19, 'Menú Mexicano', 'Incluye una selección de platos mexicanos y postre', 31.99),
-	(20, 'Menú Americano', 'Incluye una selección de platos americanos y postre', 33.99);
+INSERT INTO menu (cod_menu, nombre, descripcion, precio) VALUES (DEFAULT, 'Menú Ejecutivo', 'Incluye una selección de platos gourmet y postre', 25.99);
+INSERT INTO menu (cod_menu, nombre, descripcion, precio) VALUES (DEFAULT, 'Menú Vegetariano', 'Incluye una selección de platos vegetarianos y postre', 23.99);
+INSERT INTO menu (cod_menu, nombre, descripcion, precio) VALUES (DEFAULT, 'Menú Infantil', 'Incluye plato principal, bebida y postre para los más pequeños', 12.99);
+INSERT INTO menu (cod_menu, nombre, descripcion, precio) VALUES (DEFAULT, 'Menú Mediterráneo', 'Incluye una selección de platos mediterráneos y postre', 27.99);
+INSERT INTO menu (cod_menu, nombre, descripcion, precio) VALUES (DEFAULT, 'Menú Italiano', 'Incluye una selección de platos italianos y postre', 29.99);
+INSERT INTO menu (cod_menu, nombre, descripcion, precio) VALUES (DEFAULT, 'Menú Asiático', 'Incluye una selección de platos asiáticos y postre', 24.99);
+INSERT INTO menu (cod_menu, nombre, descripcion, precio) VALUES (DEFAULT, 'Menú Buffet', 'Incluye una variedad de platos fríos y calientes para servir estilo buffet', 32.99);
+INSERT INTO menu (cod_menu, nombre, descripcion, precio) VALUES (DEFAULT, 'Menú BBQ', 'Incluye una selección de carnes y acompañamientos para una barbacoa', 35.99);
+INSERT INTO menu (cod_menu, nombre, descripcion, precio) VALUES (DEFAULT, 'Menú de Mariscos', 'Incluye una selección de mariscos frescos y postre', 39.99);
+INSERT INTO menu (cod_menu, nombre, descripcion, precio) VALUES (DEFAULT, 'Menú Saludable', 'Incluye una selección de platos saludables y postre', 26.99);
+INSERT INTO menu (cod_menu, nombre, descripcion, precio) VALUES (DEFAULT, 'Menú Gourmet', 'Incluye una selección de platos gourmet y postre', 44.99);
+INSERT INTO menu (cod_menu, nombre, descripcion, precio) VALUES (DEFAULT, 'Menú Navideño', 'Incluye una selección de platos navideños y postre', 38.99);
+INSERT INTO menu (cod_menu, nombre, descripcion, precio) VALUES (DEFAULT, 'Menú para Fiestas', 'Incluye una variedad de platos fríos y calientes para fiestas', 29.99);
+INSERT INTO menu (cod_menu, nombre, descripcion, precio) VALUES (DEFAULT, 'Menú para Reuniones', 'Incluye una variedad de platos fríos y calientes para reuniones', 28.99);
+INSERT INTO menu (cod_menu, nombre, descripcion, precio) VALUES (DEFAULT, 'Menú para Bodas', 'Incluye una selección de platos elegantes para bodas y postre', 49.99);
+INSERT INTO menu (cod_menu, nombre, descripcion, precio) VALUES (DEFAULT, 'Menú para Cumpleaños', 'Incluye una selección de platos y postre para cumpleaños', 21.99);
+INSERT INTO menu (cod_menu, nombre, descripcion, precio) VALUES (DEFAULT, 'Menú para Aniversarios', 'Incluye una selección de platos y postre para aniversarios', 34.99);
+INSERT INTO menu (cod_menu, nombre, descripcion, precio) VALUES (DEFAULT, 'Menú de Tapas', 'Incluye una selección de tapas y postre', 19.99);
+INSERT INTO menu (cod_menu, nombre, descripcion, precio) VALUES (DEFAULT, 'Menú Mexicano', 'Incluye una selección de platos mexicanos y postre', 31.99);
+INSERT INTO menu (cod_menu, nombre, descripcion, precio) VALUES (DEFAULT, 'Menú Americano', 'Incluye una selección de platos americanos y postre', 33.99);
+
+INSERT INTO cliente (cod_cliente, nombre, apellidos, email, dni, telefono) VALUES (DEFAULT, 'Juan', 'Pérez', 'juan.perez@example.com', '12345678A', '555-1234');
+INSERT INTO cliente (cod_cliente, nombre, apellidos, email, dni, telefono) VALUES (DEFAULT,'María', 'López', 'maria.lopez@example.com', '87654321B', '555-5678');
+INSERT INTO cliente (cod_cliente, nombre, apellidos, email, dni, telefono) VALUES (DEFAULT,'Pedro', 'García', 'pedro.garcia@example.com', '98765432C', '555-9012');
+INSERT INTO cliente (cod_cliente, nombre, apellidos, email, dni, telefono) VALUES (DEFAULT,'Laura', 'Martínez', 'laura.martinez@example.com', '56789012D', '555-3456');
+INSERT INTO cliente (cod_cliente, nombre, apellidos, email, dni, telefono) VALUES (DEFAULT,'Carlos', 'Fernández', 'carlos.fernandez@example.com', '21098765E', '555-7890');
+INSERT INTO cliente (cod_cliente, nombre, apellidos, email, dni, telefono) VALUES (DEFAULT,'Ana', 'Rodríguez', 'ana.rodriguez@example.com', '54321098F', '555-2345');
+INSERT INTO cliente (cod_cliente, nombre, apellidos, email, dni, telefono) VALUES (DEFAULT,'Miguel', 'Gómez', 'miguel.gomez@example.com', '89012345G', '555-6789');
+INSERT INTO cliente (cod_cliente, nombre, apellidos, email, dni, telefono) VALUES (DEFAULT,'Sara', 'Torres', 'sara.torres@example.com', '67890123H', '555-0123');
+INSERT INTO cliente (cod_cliente, nombre, apellidos, email, dni, telefono) VALUES (DEFAULT,'Luis', 'Ramírez', 'luis.ramirez@example.com', '32109876I', '555-4567');
+INSERT INTO cliente (cod_cliente, nombre, apellidos, email, dni, telefono) VALUES (DEFAULT,'Elena', 'Hernández', 'elena.hernandez@example.com', '45678901J', '555-8901');
