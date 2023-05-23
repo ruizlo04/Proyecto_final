d81b5e88e27efb95da569edc407ad5c9b3e269f4
Carlos Ruiz López <carlos.ruiz@gmail.com>
2023-05-23 11:57:25 +0200

    Cmabios desde HacerReservas

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -1,11 +1,23 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.demo.service.ReservaServicio;
+
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import com.example.demo.model.Reserva;
 
 @Controller
+@RequestMapping("/user")
 public class MainController {
+	
+	@Autowired
+	private ReservaServicio reservaServicio;
 	
 	@GetMapping("/bodas")
 	public String mostrarBodas() {
@@ -17,10 +29,21 @@ public class MainController {
 		return "eventos";
 	}
 
-	@GetMapping("/registro")
+	@GetMapping("/registroUser")
 	public String mostrarRegistro() {
-		return "registro";
+		return "registroUser";
 	}
 	
+	@GetMapping("/nuevoUser")
+	public String mostrarFormularioUser(Model model) {
+		model.addAttribute("reserva", new Reserva());
+		return "registroUser";
+	}
+	
+	@PostMapping("/nuevoUser/submit")
+	public String procesarFormularioUser(@ModelAttribute("reserva") Reserva r) {
+		reservaServicio.add(r);
+		return "redirect:/index/"; 
+	}
 	
 }
